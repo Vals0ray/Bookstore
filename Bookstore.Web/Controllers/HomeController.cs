181b43b32c0eac6f6f795931d2027c699cbd9c41
Vals0ray <slavikdukhnych@gmail.com>
2020-01-20 20:46:53 +0200

    Modified homepage

--- Bookstore.Web/Controllers/HomeController.cs
+++ Bookstore.Web/Controllers/HomeController.cs
@@ -2,26 +2,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Bookstore.Web.Models;
 using Bookstore.BLL.Interfaces;
+using System.Collections.Generic;
+using AutoMapper;
+using System.Linq;
 
 namespace Bookstore.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly IBookService books;
+        private readonly IMapper mapper;
 
-        public HomeController(IBookService bookService)
+        public HomeController(IBookService bookService, IMapper mapper)
         {
             books = bookService;
+            this.mapper = mapper;
         }
 
         public IActionResult Index()
         {
-            return View();
-        }
+            var bookDTOs = books.Get().ToList();
 
-        public IActionResult Privacy()
-        {
-            return View();
+            if(bookDTOs.Count >= 4)
+            {
+                bookDTOs = bookDTOs.TakeLast(4).ToList();
+            }
+
+            var model = mapper.Map<List<BookViewModel>>(bookDTOs);
+
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
