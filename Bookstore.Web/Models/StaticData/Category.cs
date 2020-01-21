a4361619166fb8bc1a340eee184a94118ca736b9
Vals0ray <slavikdukhnych@gmail.com>
2020-01-21 14:52:12 +0200

    Working with the navigation bar

---
+++ Bookstore.Web/Models/StaticData/Category.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Web.Models.StaticData
+{
+    public static class Category
+    {
+        public static List<string> Categories = new List<string>()
+        {
+            "Architecture",
+            "Art",
+            "Biography & Autobiography",
+            "Body, Mind & Spirit",
+            "Business & Economics",
+            "Comics & Graphic Novels",
+            "Computers",
+            "Cooking",
+            "Crafts & Hobbies",
+            "Current Events",
+            "Drama",
+            "Education",
+            "Family & Relationships",
+            "Fiction",
+            "Foreign Language Study",
+            "Games",
+            "Gardening",
+            "Health & Fitness",
+            "History",
+            "House & Home",
+            "Humor",
+            "Juvenile Fiction",
+            "Juvenile Nonfiction",
+            "Language Arts & Disciplines",
+            "Law",
+            "Literary Collections",
+            "Literary Criticism",
+            "Mathematics",
+            "Medical",
+            "Music",
+            "Nature",
+            "Performing Arts",
+            "Pets",
+            "Philosophy",
+            "Photography",
+            "Poetry",
+            "Political Science",
+            "Psychology",
+            "Reference",
+            "Religion",
+            "Science",
+            "Self-help",
+            "Social Science",
+            "Sports & Recreation",
+            "Study Aids",
+            "Technology",
+            "Transportation",
+            "Travel",
+            "True Crime",
+        };
+    }
+}
