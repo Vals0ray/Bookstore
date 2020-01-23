11644f0334c17ce4a7faa4e1706105e94d8f7243
Vals0ray <slavikdukhnych@gmail.com>
2020-01-23 21:20:55 +0200

    Added order controller and cart view

---
+++ Bookstore.Web/Controllers/OrderController.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Bookstore.BLL.Interfaces;
+using Bookstore.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Bookstore.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IBookService books;
+        private readonly IMapper mapper;
+
+        public OrderController(IBookService bookService, IMapper mapper)
+        {
+            books = bookService;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Cart(int? id)
+        {
+            if(id != null)
+            {
+                var book = books.GetWithInclude(b => b.Id == id, b => b.Authors).First();
+                var model = mapper.Map<BookViewModel>(book);
+                var cart = Models.StaticData.Cart.BooksInCart;
+                cart.Add(model);
+                Models.StaticData.Cart.Sum += model.Price;
+            }
+
+            return View();
+        }
+
+        public IActionResult Remove(int? id)
+        {
+            if (id != null)
+            {
+                var cart = Models.StaticData.Cart.BooksInCart;
+                var cartBook = cart.First(b => b.Id == id);
+                cart.Remove(cartBook);
+                Models.StaticData.Cart.Sum -= cartBook.Price;
+            }
+
+            return View("Cart");
+        }
+    }
+}
---
+++ Bookstore.Web/Models/StaticData/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bookstore.Web.Models.StaticData
+{
+    public static class Cart
+    {
+        private static double sum = 0.00;
+        public static double Sum { get => Math.Round(sum, 2); set => sum = value; }
+        public static List<BookViewModel> BooksInCart { get; set; }
+
+        static Cart()
+        {
+            BooksInCart = new List<BookViewModel>();
+        }
+    }
+}
